c424bf04234a4afc38bdc6bbf1e855ec5b7dac6d
PrangonGhose <prangonghose@gmail.com>
2023-03-12 02:20:52 +0600

    Fix belongs-to relation

--- student_class.rb
+++ student_class.rb
@@ -6,11 +6,7 @@ class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
-  end
-
-  def add_classroom(classroom)
-    @classroom = classroom
-    classroom.add_student(self) unless classroom.students.include?(self)
+    classroom.students << self
   end
 
   # defining play_hooky method
