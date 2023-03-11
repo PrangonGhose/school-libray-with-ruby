2f173d14c8c84b7db8f95173d512759769b31187
PrangonGhose <prangonghose@gmail.com>
2023-03-12 00:58:31 +0600

    Add book class

---
+++ book.rb
@@ -0,0 +1,16 @@
+class Book
+  attr_accessor :title, :author
+  attr_reader :rentals
+
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
+
+  def add_rentals(rental)
+    @rentals.push(rental)
+    rental.book = self
+    rental.person.add_rentals(rentals) unless rental.person.rentals.include?(rental)
+  end
+end
