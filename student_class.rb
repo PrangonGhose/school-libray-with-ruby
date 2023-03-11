7d189a08d4645ffdc9b46fa6770c7c672e24559e
PrangonGhose <prangonghose@gmail.com>
2023-03-12 00:58:59 +0600

    Update student class

--- student_class.rb
+++ student_class.rb
@@ -1,9 +1,16 @@
 require_relative 'person_class'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
+  end
+
+  def add_classroom(classroom)
+    @classroom = classroom
+    classroom.add_student(self) unless classroom.students.include?(self)
   end
 
   # defining play_hooky method
