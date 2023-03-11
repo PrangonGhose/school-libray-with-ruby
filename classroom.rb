fa9359a50bbd9e395c083657758afb8d3b55cb65
PrangonGhose <prangonghose@gmail.com>
2023-03-12 02:20:23 +0600

    Fix has-a relation

--- classroom.rb
+++ classroom.rb
@@ -1,14 +1,13 @@
 class Classroom
-  attr_accessor :label
-  attr_reader :student
+  attr_accessor :label, :student
 
   def initialize(label)
     @label = label
     @students = []
   end
 
-  def add_student(student)
+  def add_student(age, name = 'Unknown', parent_permission: true)
+    student = Student.new(age, self, name, parent_permission)
     @students.push(student)
-    student.classroom = self
   end
 end
