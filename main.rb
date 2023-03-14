38f43871404d033077acdb0496dddb7e5645142f
rmrmrmr <ricardomartineza77@gmail.com>
2023-03-14 14:19:08 -0600

    Refactor app.rb & main.rb with modules

--- main.rb
+++ main.rb
@@ -1,29 +1,33 @@
 require_relative 'app'
-require_relative 'run_menu'
+require_relative './modules/run_menu'
 
-def menu
-  puts "\n-----------------------------------\nPick an option by entering a number:"
-  print("
-  1 - List all books
-  2 - List all people
-  3 - Add a book
-  4 - Add a person
-  5 - Rent a book
-  6 - List rents of a person
-  0 - Exit
-  ")
-end
+class Main
+  include RunMenu
 
-def main
-  option = -1
-  app = App.new
-  until option.zero?
-    menu
-    option = gets.chomp.to_i
-    puts ''
-    RunMenu.new(app, option).run_menu unless option.zero?
+  def menu
+    puts "\n-----------------------------------\nPick an option by entering a number:"
+    print("
+    1 - List all books
+    2 - List all people
+    3 - Add a book
+    4 - Add a person
+    5 - Rent a book
+    6 - List rents of a person
+    0 - Exit
+    ")
   end
-  puts 'Thank you for using this app'
+
+  def main
+    option = -1
+    app = App.new
+    until option.zero?
+      menu
+      option = gets.chomp.to_i
+      puts ''
+      run_menu(app, option) unless option.zero?
+    end
+    puts 'Thank you for using this app'
+  end
 end
 
-main
+Main.new.main
